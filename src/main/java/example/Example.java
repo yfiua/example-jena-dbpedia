06dc2e0f419b27d631fa89a2ff8d0d1fadfe0fc2
Jun Sun <yfiua@qq.com>
2017-01-10 13:17:55 +0100

    add mvn properties

--- src/main/java/example/Example.java
+++ src/main/java/example/Example.java
@@ -4,8 +4,7 @@ import org.apache.jena.query.* ;
 import org.apache.jena.sparql.engine.http.QueryEngineHTTP ;
 
 public class Example {
-	static public void main(String...argv)
-    {
+	static public void main(String...argv) {
         String queryStr = "SELECT ?prop ?place WHERE { <http://dbpedia.org/resource/%C3%84lvdalen> ?prop ?place .}";
         Query query = QueryFactory.create(queryStr);
 
