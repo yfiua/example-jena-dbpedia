f559ebcfbb14f7bea14b4234f1d4ae0f4c606e6a
yfiua <yfiua@qq.com>
2017-01-10 13:20:04 +0100

    format

--- src/main/java/example/Example.java
+++ src/main/java/example/Example.java
@@ -1,10 +1,10 @@
 package example;
 
-import org.apache.jena.query.* ;
-import org.apache.jena.sparql.engine.http.QueryEngineHTTP ;
+import org.apache.jena.query.*;
+import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 
 public class Example {
-	static public void main(String...argv) {
+    static public void main(String...argv) {
         String queryStr = "SELECT ?prop ?place WHERE { <http://dbpedia.org/resource/%C3%84lvdalen> ?prop ?place .}";
         Query query = QueryFactory.create(queryStr);
 
